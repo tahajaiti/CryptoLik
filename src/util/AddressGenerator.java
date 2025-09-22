0727509ad3e67ecddac135c0b78f8387cb12e448
taha jaiti <kyojin.rar@gmail.com>
2025-09-22 14:52:12 +0100

    FEAT: CRYP-13 improved the address generator implementation

--- src/util/AddressGenerator.java
+++ src/util/AddressGenerator.java
@@ -6,43 +6,39 @@ import java.security.SecureRandom;
 
 public class AddressGenerator {
     private static final String BASE58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-    private static final SecureRandom random = new SecureRandom();
+    private static final SecureRandom RANDOM = new SecureRandom();
 
     public static String generateAddress(WalletType type){
-        if(type == WalletType.BITCOIN){
-            return generateBitcoinAddress();
-        } else if (type == WalletType.ETHEREUM) {
-            return generateEthereumAddress();
-        } else {
-            throw new IllegalArgumentException("Unsupported wallet type: " + type);
+        switch (type){
+            case BITCOIN: return generateBitcoinAddress();
+            case ETHEREUM: return generateEthereumAddress();
+            default: throw new IllegalArgumentException("Unsupported wallet type: " + type);
         }
     }
 
     private static String generateBitcoinAddress(){
-        StringBuilder address = new StringBuilder("1");
-        for (int i = 0; i < 33; i++) {
-            int index = random.nextInt(BASE58.length());
-            address.append(BASE58.charAt(index));
-        }
+        StringBuilder address = new StringBuilder();
+        int choice = RANDOM.nextInt(3);
 
-        int choice = random.nextInt(3);
         if (choice == 0) {
-            address.setCharAt(0, '1');
+            address.append('1');
         } else if (choice == 1) {
-            address.setCharAt(0, '3');
+            address.append('3');
         } else {
-            address.setCharAt(0, 'b');
-            address.insert(1, 'c');
+            address.append("bc1");
         }
 
+        int length = (choice == 2 ? 32 : 33);
+        for (int i = 0; i < length; i++) {
+            address.append(BASE58.charAt(RANDOM.nextInt(BASE58.length())));
+        }
         return address.toString();
     }
 
     private static String generateEthereumAddress(){
         StringBuilder address = new StringBuilder("0x");
         for (int i = 0; i < 40; i++) {
-            int index = random.nextInt(16);
-            address.append(Integer.toHexString(index));
+            address.append(Integer.toHexString(RANDOM.nextInt(16)));
         }
         return address.toString();
     }
