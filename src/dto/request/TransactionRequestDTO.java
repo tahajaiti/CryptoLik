389800e6e371583cfdf53f71e99fed2f78f796c7
taha jaiti <kyojin.rar@gmail.com>
2025-09-23 16:21:48 +0100

    FEAT: CRYP-57 created the transaction request and response dto

---
+++ src/dto/request/TransactionRequestDTO.java
@@ -0,0 +1,39 @@
+package dto.request;
+
+import entity.enums.WalletType;
+
+public class TransactionRequestDTO {
+    private String srcAddress;
+    private String destAddress;
+    private double amount;
+
+    public TransactionRequestDTO(String srcAddress, String destAddress, double amount) {
+        this.srcAddress = srcAddress;
+        this.destAddress = destAddress;
+        this.amount = amount;
+    }
+
+    public String getSrcAddress() {
+        return srcAddress;
+    }
+
+    public void setSrcAddress(String srcAddress) {
+        this.srcAddress = srcAddress;
+    }
+
+    public String getDestAddress() {
+        return destAddress;
+    }
+
+    public void setDestAddress(String destAddress) {
+        this.destAddress = destAddress;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
