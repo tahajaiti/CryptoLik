bb47e3afbb6ebbb76c500963c31be38a382cf384
taha jaiti <kyojin.rar@gmail.com>
2025-09-24 10:00:27 +0100

    FEAT: CRYP-63 created the authservice interface

---
+++ src/service/interfaces/AuthService.java
@@ -0,0 +1,13 @@
+package service.interfaces;
+
+import dto.request.AuthRequestDTO;
+import dto.request.WalletRequestDTO;
+import dto.response.AuthResponseDTO;
+import dto.response.WalletResponseDTO;
+
+public interface AuthService {
+    WalletResponseDTO register(WalletRequestDTO requestDTO);
+    AuthResponseDTO login(AuthRequestDTO requestDTO);
+    void logout(String token);
+    WalletResponseDTO getCurrentUser(String token);
+}
