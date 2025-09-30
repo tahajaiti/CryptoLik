f473d93e853dd257d15002fdf79ac1a681230131
taha jaiti <kyojin.rar@gmail.com>
2025-09-30 23:38:10 +0100

    FEAT: improved the logging class to output into a file

--- src/util/Log.java
+++ src/util/Log.java
@@ -1,37 +1,82 @@
 package util;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.logging.*;
 
 public class Log {
-    
 
-    private Log() {
+    private static Logger logger;
+    private static boolean initialized = false;
 
-    }
+    private static final String LOG_DIR = "logs";
+    private static final String LOG_FILE_PREFIX = "app";
+    private static final String LOG_FILE_EXTENSION = ".log";
+    private static final boolean APPEND = false; // Overwrite the log file each time
 
-    private static Logger logger(Class<?> c) {
-        return Logger.getLogger(c.getName());
+
+    private Log() { }
+
+    private static synchronized Logger getLogger() {
+        if (initialized) return logger;
+
+        logger = Logger.getLogger("AppLogger");
+        logger.setUseParentHandlers(false); // Disable console logging
+
+        for (Handler h : logger.getHandlers()) {
+            logger.removeHandler(h);
+        }
+
+        try {
+            File dir = new File("logs");
+            if (!dir.exists()) dir.mkdirs();
+
+            FileHandler fh = new FileHandler(
+                    LOG_DIR + File.separator + LOG_FILE_PREFIX + "_" +
+                    LocalDateTime.now().toString().replace(":", "-") + LOG_FILE_EXTENSION,
+                    APPEND
+            );
+            fh.setFormatter(new SimpleFormatter() {
+                @Override
+                public synchronized String format(LogRecord logRecord) {
+                    return String.format("[%1$tF %1$tT] [%2$s] [%3$s] %4$s%n",
+                            logRecord.getMillis(),
+                            logRecord.getLevel(),
+                            logRecord.getLoggerName(),
+                            logRecord.getMessage()
+                    );
+                }
+            });
+
+            logger.addHandler(fh);
+            initialized = true;
+
+        } catch (Exception e) {
+            logger.warning("Failed to initialize logger: " + e.getMessage());
+        }
+
+        return logger;
     }
 
     public static void debug(Class<?> c, String msg) {
-        Logger l = logger(c);
-        if (l.isLoggable(Level.FINE)) l.fine(msg);
+        getLogger().fine("[" + c.getSimpleName() + "] " + msg);
     }
 
     public static void info(Class<?> c, String msg) {
-        logger(c).info(msg);
+        getLogger().info("[" + c.getSimpleName() + "] " + msg);
     }
 
     public static void warn(Class<?> c, String msg) {
-        logger(c).warning(msg);
+        getLogger().warning("[" + c.getSimpleName() + "] " + msg);
     }
 
     public static void error(Class<?> c, String msg) {
-        logger(c).severe(msg);
+        getLogger().severe("[" + c.getSimpleName() + "] " + msg);
     }
 
     public static void error(Class<?> c, String msg, Throwable t) {
-        logger(c).log(Level.SEVERE, msg, t);
+        LogRecord rec = new LogRecord(Level.SEVERE, "[" + c.getSimpleName() + "] " + msg);
+        rec.setThrown(t);
+        getLogger().log(rec);
     }
 }
