aeb743524e2f9e163e646527a9b977c7132cdb4e
taha jaiti <kyojin.rar@gmail.com>
2025-09-30 23:52:40 +0100

    FEAT: CRYP-70 added the config class for the mempool

---
+++ src/config/MempoolConfig.java
@@ -0,0 +1,10 @@
+package config;
+
+public class MempoolConfig {
+    public static final int MAX_MEMPOOL_SIZE = 50;
+    public static final int PROCESS_BATCH_SIZE = 1;
+    public static final int PROCESS_INTERVAL_MS = 15000;
+
+    private MempoolConfig() {
+    }
+}
