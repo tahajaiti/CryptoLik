e25e1a84635696a5b40cd3ba38fbe975d80e60f3
taha jaiti <kyojin.rar@gmail.com>
2025-09-30 23:43:35 +0100

    FEAT: CRYP-30 fixed issues with the repository interfaces

--- src/repository/interfaces/Repository.java
+++ src/repository/interfaces/Repository.java
@@ -7,6 +7,6 @@ public interface Repository<T, ID> {
     T save(T entity);
     Optional<T> findById(ID id);
     List<T> findAll();
-    void update(T entity);
+    T update(T entity);
     void delete(ID id);
 }
--- src/repository/interfaces/TransactionRepository.java
+++ src/repository/interfaces/TransactionRepository.java
@@ -1,9 +1,11 @@
 package repository.interfaces;
 
 import entity.Transaction;
+import entity.enums.TransactionStatus;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository extends Repository<Transaction, UUID>{
-    
+    List<Transaction> findByStatus(TransactionStatus status);
 }
