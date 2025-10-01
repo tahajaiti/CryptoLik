d9c91c59f960b57b6e93d32ea881f626c84464d5
taha jaiti <kyojin.rar@gmail.com>
2025-10-01 11:58:49 +0100

    FEAT: added 2 wallets by default

--- resources/sql/01_wallet.sql
+++ resources/sql/01_wallet.sql
@@ -18,3 +18,14 @@ CREATE TABLE IF NOT EXISTS wallets (
 CREATE INDEX IF NOT EXISTS idx_wallet_type ON wallets(type);
 CREATE INDEX IF NOT EXISTS idx_wallet_address ON wallets(address);
 CREATE INDEX IF NOT EXISTS idx_wallet_id ON wallets(id);
+
+DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM wallets WHERE address = 'bc1MxK2QsDf4wp3bdXe7DPHe5uEJuHLdEjTS4') THEN
+            INSERT INTO wallets (type, address, balance, password)
+            VALUES
+                ('BITCOIN', 'bc1MxK2QsDf4wp3bdXe7DPHe5uEJuHLdEjTS4', 10000, 'password'),
+                ('ETHEREUM', '0xjdsLDAW3sOd3dc9km2Fl21l04eJzNX02pel08', 10000, 'password');
+        END IF;
+    END
+$$;
